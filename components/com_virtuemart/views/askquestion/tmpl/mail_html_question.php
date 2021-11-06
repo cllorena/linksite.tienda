e8309cc5dbff1d6ad083053ba8a31d9297c12ea5
cllorena <claudio.llorena@gmail.com>
2021-11-06 20:59:33 -0300

    1008

--- components/com_virtuemart/views/askquestion/tmpl/mail_html_question.php
+++ components/com_virtuemart/views/askquestion/tmpl/mail_html_question.php
@@ -65,7 +65,8 @@ defined('_JEXEC') or die('');
 										?>
 										<td width="30%" align="center">
 											<a href="<?php  echo $product_link ?>">
-											<?php echo $this->product->images[0]->displayMediaThumb('',false,'',true,false,true); ?>
+											<?php //echo $this->product->images[0]->displayMediaThumb('',false,'',true,false,true); ?>
+											<?php echo $this->product->images[0]->displayMediaThumb('style="width: 50%;" ',false,'',true,false,true); ?>
 											</a>
 										</td>
 										<td width="70%" valign="top">
